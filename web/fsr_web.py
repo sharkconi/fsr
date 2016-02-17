473ba51309f77877dacd085b9e12df5b2b655736
unknown <mam22@CNENMAM22L2C.corp.emc.com>
2016-02-17 21:54:44 +0800

    Support for both web and mobile page

--- web/fsr_web.py
+++ web/fsr_web.py
@@ -4,6 +4,8 @@
 from flask import Flask, render_template, redirect, url_for
 import MySQLdb
 from decimal import *
+import datetime
+import time
 
 app = Flask(__name__)
 
@@ -20,6 +22,7 @@ def handle_ribao_data(res):
 
 @app.route('/m')
 def fsr_mobile_page():
+    date_str = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
     conn = MySQLdb.connect(
         host = 'rds3vt9mpg9qj5biy748a.mysql.rds.aliyuncs.com',
         port = 3306,
@@ -30,15 +33,24 @@ def fsr_mobile_page():
     )
     cur = conn.cursor()
     
-    result = cur.execute("select * from ribao where date = \'2015-12-03\'")
+    result = cur.execute("select * from ribao where date = \'" + date_str + "\'")
     ds, all_in = handle_ribao_data(cur.fetchmany(result))
     cur.close()
     conn.close()
  
-    return render_template('fsr_mobile.html', ds = ds, d="2015-12-03", all_in='{:6,.2f}'.format(all_in/10000))
+    return render_template('fsr_mobile.html', ds = ds, d=date_str, all_in='{:6,.2f}'.format(all_in/10000))
 
-@app.route('/web')
-def fsr_web_page():
+@app.route('/web/<date_str>')
+def fsr_web_page_date(date_str):
+    date_split=date_str.split('-')
+    date_now = datetime.datetime(year=int(date_split[0]), month=int(date_split[1]), day=int(date_split[2]))
+
+    date_str_list = [(date_now).strftime("%Y-%m-%d"),
+           (date_now - datetime.timedelta(days=1)).strftime("%Y-%m-%d"),
+           (date_now - datetime.timedelta(days=2)).strftime("%Y-%m-%d"),
+           (date_now - datetime.timedelta(days=3)).strftime("%Y-%m-%d"),
+           (date_now - datetime.timedelta(days=4)).strftime("%Y-%m-%d"),
+           (date_now - datetime.timedelta(days=5)).strftime("%Y-%m-%d"),]
     conn = MySQLdb.connect(
         host = 'rds3vt9mpg9qj5biy748a.mysql.rds.aliyuncs.com',
         port = 3306,
@@ -46,20 +58,24 @@ def fsr_web_page():
         passwd = 'fsradmin',
         db = 'fsr',
         charset = 'utf8',
-        connect_timeout = 5,
     )
     cur = conn.cursor()
     
-    result = cur.execute("select * from ribao where date = \'2015-12-03\'")
-    ds = cur.fetchmany(result)
+    result = cur.execute("select * from ribao where date = \'" + date_str_list[0] + "\'")
+    ds, all_in = handle_ribao_data(cur.fetchmany(result))
     cur.close()
     conn.close()
  
-    return render_template('fsr_web.html', ds = ds)
+    return render_template('fsr_web.html', ds = ds, d=date_str_list, all_in='{:6,.2f}'.format(all_in/10000))
+
+@app.route('/web')
+def fsr_web_page_main():
+    date_now = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    return redirect(url_for("fsr_web_page_date", date_str=date_now))
 
 @app.route("/")
 def fsr_main():
-    return redirect(url_for('fsr_web_page'))
+    return redirect(url_for("fsr_web_page_main"))
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug = True)
